f4056902d56e08e6c9528688d798c529ac0c90f5
Basavaraj <basavarajningasani123@gmail.com>
2026-02-21 09:39:16 +0530

    syntax fix

--- renderer.js
+++ renderer.js
@@ -262,7 +262,7 @@ function loadNewTab() {
 
   const content = document.getElementById("content");
   content.innerHTML = `
-    <iframe
+  <iframe
       src="./pages/newtab.html"
       style="width:100%; height:100%; border:none;">
     </iframe>
